4e3b17772d06e84c589c4a9d7af8f2652013090c
unknown <heldheldheldheldheldheldheld@gmail.com>
2025-12-19 02:52:26 +0100

    H1

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -2,4 +2,24 @@ package h1;
 
 public class H1_Main {
 
+	public static void main(String[] args) {
+		PrioListe list = new PrioListe();
+		Patient a = new Patient("Kurt",1);
+		Patient b = new Patient("Bernd",10);
+		Patient c = new Patient("Bastian",5);
+		Patient d = new Patient("Karl",7);
+		list.addPatient(a);
+		list.addPatient(b);
+		list.addPatient(c);
+		list.addPatient(d);
+		System.out.println(list.getNextPatient().name);
+		System.out.println(a.name + ": " + list.getPosition(a));
+		if(!list.myList.isEmpty()) {
+			for(Patient p : list.myList) {
+				System.out.print(p.name + "-->");
+			}
+			System.out.print("||");
+		}
+		
+	}
 }
---
+++ src/h1/PrioListe.java
@@ -0,0 +1,31 @@
+package h1;
+import java.util.ArrayList;
+
+public class PrioListe {
+	public ArrayList<Patient> myList = new ArrayList<Patient>();
+	
+	public void addPatient(Patient p) {
+		if(myList.isEmpty()) myList.add(p);
+		else if(p.prio > 0) {
+			int index = myList.size();
+			for(Patient a : myList) {
+				if((p.prio < a.prio) && myList.indexOf(a) < index) {
+					index = myList.indexOf(a);
+				}
+			}
+			myList.add(index, p);
+		}
+	}
+	
+	public Patient getNextPatient() {
+		if(!myList.isEmpty()) {
+			return myList.removeFirst();		
+		}else {
+			return null;
+		}
+	}
+	
+	public int getPosition(Patient p) {
+		return myList.indexOf(p);
+	}
+}
